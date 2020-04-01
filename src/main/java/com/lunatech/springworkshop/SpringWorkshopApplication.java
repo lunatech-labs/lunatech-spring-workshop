ca357340eed5bb7ac6098f5fb5cadaf7e0538064
Diego Román Rodríguez <diego.roman@lunatech.nl>
2020-04-01 13:19:45 +0200

    changed project group-id

--- src/main/java/com/lunatech/springworkshop/SpringWorkshopApplication.java
+++ src/main/java/com/lunatech/springworkshop/SpringWorkshopApplication.java
@@ -1,4 +1,4 @@
-package lunatechlabs.springworkshop;
+package com.lunatech.springworkshop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/com/lunatech/springworkshop/SpringWorkshopApplicationTests.java
+++ src/test/java/com/lunatech/springworkshop/SpringWorkshopApplicationTests.java
@@ -1,4 +1,4 @@
-package lunatechlabs.springworkshop;
+package com.lunatech.springworkshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
